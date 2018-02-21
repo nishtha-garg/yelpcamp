67a0974121dde3f2d540a36f1044cf531e27a0ed
Nishtha Garg <ngarg5@hawk.iit.edu>
2018-02-21 02:18:28 +0000

    app.js final version

---
+++ app.js
@@ -0,0 +1,98 @@
+var express=require("express");
+require('dotenv').load();
+var app=express();
+
+app.set("view engine", "ejs");
+const ejsLint = require('ejs-lint');
+
+ ejsLint.lint();
+ require('dotenv').config()
+var bodyparser=require("body-parser");
+app.use(bodyparser.urlencoded({extended:true}));
+
+
+//to be used before passport config
+var flash=require("connect-flash");
+app.use(flash());
+
+app.use(express.static(__dirname+"/public"));
+
+var passport=require("passport");
+var LocalStrategy=require("passport-local");
+
+
+
+
+
+//add mongoose
+var mongoose=require("mongoose");
+mongoose.connect("mongodb://localhost/yelp_camp");
+
+
+
+
+
+// //Schema Setup
+// var campgroundSchema=new mongoose.Schema({
+//     name:String,
+//     image:String,
+//     description:String
+// });
+
+//modelling
+var Campground=require("./models/campground");
+var Comment=require("./models/comment")
+var seedDB=require("./seeds")
+var User=require("./models/user");
+
+var methodOverride=require("method-override");
+app.use(methodOverride("_method"));
+
+
+
+var commentRoutes=require("./routes/comments");
+var campgroundRoutes=require("./routes/campgrounds");
+var indexRoutes=require("./routes/index");
+
+app.locals.moment = require('moment');
+
+
+// seedDB();
+
+/////////////////////////////
+//PASSPORT CONFIGURATION//
+//////////////////////////
+
+app.use(require("express-session")({
+    secret:"Rusty is the cutest dog ever",
+    resave:false,
+    saveUnintialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req,res,next){
+    res.locals.currentUser=req.user;
+    res.locals.success=req.flash("success");
+    res.locals.error=req.flash("error");
+    next();
+});
+
+
+app.get("/", function(req,res){
+    res.render("landing.ejs")
+})
+
+
+app.use(indexRoutes);
+app.use(campgroundRoutes);
+app.use(commentRoutes);
+ 
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("The yelp camp server has started");
+});
